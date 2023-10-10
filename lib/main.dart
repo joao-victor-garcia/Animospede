3f7348698f0b9e4cea07cff99d726abee0013295
mattsz <6matheuslima@gmail.com>
2023-10-09 21:43:03 -0300

    Feat: login screen pt 1

--- lib/main.dart
+++ lib/main.dart
@@ -5,14 +5,15 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Animóspede',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Color.fromARGB(255, 253, 171, 94)),
+        colorScheme:
+            ColorScheme.fromSeed(seedColor: Color.fromARGB(255, 253, 171, 94)),
         useMaterial3: true,
       ),
       home: const MyHomePage(title: 'Animóspede'),
@@ -30,13 +31,66 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  Color buttonColor = const Color(0xffFF8E54);
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-      )
-    );
+        body: Stack(
+      children: [
+        Container(
+          decoration: const BoxDecoration(
+            image: DecorationImage(
+              fit: BoxFit.cover,
+              image: AssetImage('loginBackground.png'),
+            ),
+          ),
+          child: Stack(children: [
+            Container(
+              color: Colors.black.withOpacity(0.7), // Cor preta com opacidade
+              width: double.infinity,
+              height: double.infinity,
+            ),
+            Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  ElevatedButton(
+                    onPressed: () {},
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: buttonColor,
+                      foregroundColor: Colors.white,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(20),
+                      ),
+                      minimumSize: const Size(350, 60),
+                    ),
+                    child: const Text('Login'),
+                  ),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  ElevatedButton(
+                    onPressed: () {},
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: buttonColor,
+                      foregroundColor: Colors.white,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(20),
+                      ),
+                      minimumSize: const Size(350, 60),
+                    ),
+                    child: const Text('Register'),
+                  ),
+                ],
+              ),
+            ),
+          ]),
+        )
+      ],
+    ));
   }
 }
